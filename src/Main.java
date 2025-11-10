ad9c552c85b1efa95632da475818fbd5dbf3471b
ShaanSingh <shaans6@nycstudents.net>
2025-11-10 15:34:27 -0500

    Add check

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        File f = new File("Scoreboard.txt");
+        Scanner s = new Scanner(f);
+        String teams = "";
+        Team[] teams_lst = new Team[7];
+        int index = 0;
+        while (s.hasNextLine()) {
+            for (int i = 0; i < 2; i++) {
+                String x = s.next();
+                if (!teams.contains(x)) {
+                    teams_lst[index] = new Team(x);
+                    teams += x;
+                    index++;
+                }
+            }
+            s.nextLine();
+        }
+        System.out.println(teams_lst[6]);
+        s.close();
+        s = new Scanner(f);
+
+        while (s.hasNext()){
+            Team a = findTeam(s.next(), teams_lst);
+            Team b = findTeam(s.next(), teams_lst);
+            int active_choice = 0;
+
+            Team active = a;
+            for (int i = 0; i< 1000; i++){
+                if (!active.play(s.nextInt())){
+                    active_choice ++;
+                    if (active_choice %2 ==0) active = a;
+                    else active = b;
+                }
+            }
+            if (a.getPoints() > b.getPoints()) a.addWins();
+            if (b.getPoints() > a.getPoints()) b.addWins();
+
+        }
+
+    }
+
+    public static Team findTeam(String name, Team[] lst){
+        for(int i = 0; i < lst.length; i++ ){
+            if (lst[i].getName().equals(name)) return lst[i];
+        }
+        return null;
+    }
+}
---
+++ src/Team.java
@@ -0,0 +1,34 @@
+public class Team {
+
+    private String name =" ";
+    private int  points =0;
+    private int wins =0;
+
+    public Team(String name){
+        this.name = name;
+    }
+
+    public boolean play(int points){
+        if (points >0) {
+            this.points ++;
+            return true;
+        }
+        return false;
+    }
+
+    public void addWins(){
+        wins++;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getWins(){
+        return wins;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+}
